c0d6e7645fe7a6505337601b3ff37220dfc4be36
Erick <erickmachado050@gmail.com>
2026-05-14 23:27:31 -0300

    Deletando dados

---
+++ 16_PHP_E_MYSQL/11_deletando_dados/index.php
@@ -0,0 +1,22 @@
+<?php
+
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db = "cursophp";
+
+    $conn = new mysqli($host, $user,$pass, $db,);
+
+    $nome = "Mouse Gamer";
+
+    $stmt = $conn->prepare("DELETE FROM itens WHERE nome = ?");
+
+    $stmt->bind_param("s", $nome);
+
+    $stmt->execute();
+
+
+
+
+
+    $conn->close();
